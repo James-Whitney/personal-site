ca8b2cf837e1427cec2f53a2e1e53d5164aadd28
James Whitney <contact@jaswhitney.com>
2023-02-11 16:00:03 -0800

    .

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,14 +4,13 @@ module.exports = {
     "./app/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
-
     // Or if using `src` directory:
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    fontFamily: {
-      mono: ["Space Mono", "monospace"],
-    },
+    // fontFamily: {
+    //   mono: ["Space Mono", "monospace"],
+    // },
     extend: {
       fontSize: {
         "dynamic-small": "clamp(1rem, 5vw, 10rem)",
